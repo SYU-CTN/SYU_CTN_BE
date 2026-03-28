1301ae0e6749e2dabcab55ed4133ec6f7af8136d
doyeonie <doyeonie63@gmail.com>
2026-03-28 09:58:37 +0900

    Initial commit: Backend basic structure setup

---
+++ src/main/java/com/example/syu_ctn_be/SyuCtnBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.syu_ctn_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyuCtnBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyuCtnBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/syu_ctn_be/SyuCtnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syu_ctn_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyuCtnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
